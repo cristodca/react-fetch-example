d9a7726f52e5d09e810874834d4905b4eb70545d
@cristodca (Cristopher D. Chavez) <cristodca.studio@gmail.com>
2022-08-26 23:53:54 -0500

    Clase 53 | Adding a Loading/Pending State

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -6,13 +6,15 @@ import './TripList.css'
 
 export default function TripList() {
   const [url, setUrl] = useState('http://localhost:3000/trips')
-  const { data } = useFetch(url)
+  const { data, isPending } = useFetch(url)
 
 
 
   return (
     <div className='trip-list'>
       <h2>Trip List</h2>
+
+      {isPending && <div>Loading trips...</div>}
 
       <ul>
         {data && data.map(trip => (
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,19 +2,23 @@ import { useEffect, useState } from 'react'
 
 const useFetch = (url) => {
   const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsPending(true)
+      
       const response = await fetch(url)
       const json = await response.json()
       
+      setIsPending(false)
       setData(json)
     }
 
     fetchData()
   }, [url]);
 
-  return { data }
+  return { data, isPending }
 }
 
 export { useFetch }
